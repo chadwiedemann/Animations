39b33c98778cc74a2e06065802cdb741a36062a8
Chad Wiedemann <chad.wiedemann@gmail.com>
2016-09-26 10:16:54 -0400

    First commit black project

---
+++ Animations/Animations/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Animations
+//
+//  Created by Chad Wiedemann on 9/26/16.
+//  Copyright © 2016 Chad Wiedemann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
